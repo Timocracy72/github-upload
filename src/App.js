b8d296dfe30cc8900bebeb4ba30f5e6efdc58337
Timothy Martin <tmartin@impactgrouphr.com>
2020-09-24 20:23:48 -0500

    R2R - Fundamentals - Lists

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
 import React from 'react';
 
-function getTitle(title) {
-    return "Your Royal Highness Extreme Potentate, Sir " + title + " IV";
-}
-
-const radius = 5;
-const base = 6;
-const height = 7;
-const baseLength = 8;
-const baseWidth = 9;
-const area = {
-    circle: function(radius) {return Math.PI * radius * radius;},
-    rightTriangle: function(base,height) {return base * height / 2;}
-}
-const volume = {
-    pyramid4: function(baseLength, baseWidth, height) {return baseLength * baseWidth / 3 * height;},
-    sphere: function(radius) {return 4/3 * Math.PI * radius * radius * radius;}
-}
-
-const bool = true;
-var undef;
-var nu = null;
-var nan = 1 / "zero";
-
-const a = ["one","banana","yearning","declare","Neptune","utilitarianism","6,492.7","spacetime","obsequious","mauve","cassowary","Basil Rathbone"];
+const list = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
 function App() {
     return (
         <div>
-            <h1>Hello {getTitle('Porpington')}</h1>
-
-            <div>
-                Circle area (radius {radius}): {area.circle(radius)}<br />
-                Right triangle area (base {base}, height {height}): {area.rightTriangle(base,height)}<br />
-                4-sided pyramid volume (base length {baseLength}, base width {baseWidth}, height {height}): {volume.pyramid4(baseLength,baseWidth,height)}<br />
-                Sphere volume (radius {radius}): {volume.sphere(radius)}<br />
-                true: {bool} ({(bool) ? "true" : "false"})<br />
-                undefined: {undef}< br/>
-                null: {nu}<br />
-                nan: {nan}<br />
-                array: {a.join(" - ")}
-            </div>
+            <h1>My Hacker Stories</h1>
+    
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" />
+    
+            <hr />
+
+            {list.map(function(item){
+                return (
+                    <div key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </div>
+                );
+            })}
         </div>
     );
 }
